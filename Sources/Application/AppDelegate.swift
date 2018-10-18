462d593928abf2b8d99a61061404243de346f42e
Nikita Khomitsevich <hamsternik9@gmail.com>
2018-10-18 13:21:18 +0300

    Initial commit: create base project with default structure

---
+++ Sources/Application/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  YourAnimeList-iOS
+//
+//  Created by Nikita Khomitsevich on 10/18/18.
+//  Copyright © 2018 Nikita Khomitsevich. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool
+    {
+        return true
+    }
+
+}
---
+++ Sources/HomeList/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  YourAnimeList-iOS
+//
+//  Created by Nikita Khomitsevich on 10/18/18.
+//  Copyright © 2018 Nikita Khomitsevich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
